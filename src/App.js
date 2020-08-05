cc40e492aea0fe9e013ff6f1b56deb7a7320c198
Chienlee kuo <chienleekuo@Chienlees-MBP.socal.rr.com>
2020-08-04 18:17:02 -0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { withRouter } from "react-router";
+import { Layout } from "antd";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { ApolloProvider as ApolloHooksProvider } from "@apollo/react-hooks";
+import { ApolloProvider } from "react-apollo";
+import AWSAppSyncClient, { AUTH_TYPE } from "aws-appsync";
+import { Rehydrated } from "aws-appsync-react";
+import cubejs from "@cubejs-client/core";
+import { CubeProvider } from "@cubejs-client/react";
+import { withAuthenticator } from "aws-amplify-react";
+import Amplify, { Auth, Hub } from 'aws-amplify';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Header from './components/Header';
+import aws_exports from './aws-exports';
 
-export default App;
+const API_URL = "http://localhost:4000";
+const cubejsApi = cubejs(
+  async () => (await Auth.currentSession()).getIdToken().getJwtToken(),
+  { apiUrl: `${API_URL}/cubejs-api/v1` }
+);
+
+Amplify.configure(aws_exports);
+
+const client = new AWSAppSyncClient(
+  {
+    disableOffline: true,
+    url: aws_exports.aws_appsync_graphqlEndpoint,
+    region: aws_exports.aws_appsync_region,
+    auth: {
+      type: AUTH_TYPE.AMAZON_COGNITO_USER_POOLS,
+      jwtToken: async () => (await Auth.currentSession()).getIdToken().getJwtToken()
+    },
+  },
+  { cache: new InMemoryCache() }
+);
+
+Hub.listen('auth', (data) => {
+  if (data.payload.event === 'signOut') {
+    client.resetStore();
+  }
+});
+
+const AppLayout = ({ location, children }) => (
+  <Layout style={{ height: "100%" }}>
+    <Header location={location} />
+    <Layout.Content>{children}</Layout.Content>
+  </Layout>
+);
+
+const App = withRouter(({ location, children }) => (
+  <CubeProvider cubejsApi={cubejsApi}>
+    <ApolloProvider client={client}>
+      <ApolloHooksProvider client={client}>
+        <Rehydrated>
+          <AppLayout location={location}>{children}</AppLayout>
+        </Rehydrated>
+      </ApolloHooksProvider>
+    </ApolloProvider>
+  </CubeProvider>
+));
+
+export default withAuthenticator(App, {
+  signUpConfig: {
+    hiddenDefaults: ["phone_number"]
+  }
+});
